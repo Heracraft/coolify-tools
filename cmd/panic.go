1b36cb1a1488f20963b433fed8b9c63e961866d5
Heracraft <nehemiahelibariki@gmail.com>
2026-05-22 03:22:28 -0400

    chore: remove boilerplate comments

--- cmd/panic.go
+++ cmd/panic.go
@@ -6,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// panicCmd represents the panic command
 var panicCmd = &cobra.Command{
 	Use:   "panic",
 	Short: "Restore on autopilot, no questions just try all defaults to restore an instance. useful when you are loosing your shit",
@@ -18,14 +17,4 @@ var panicCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(panicCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// panicCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// panicCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,4 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 
-*/
 package cmd
 
 import (
@@ -12,18 +9,12 @@ import (
 
 
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "coolify-cli",
 	Short: "tooling for using coolify in prod. Starting with instance wide backups",
 	Long: `Tools for those deploying coolify in production. For now that's instance wide backups encrypted with an SSH key using age`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -32,14 +23,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.coolify-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
